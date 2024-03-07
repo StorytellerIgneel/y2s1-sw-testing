a67040fb433da5b83a26a6a22b8bd0fcf2186629
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-03-07 20:37:44 +0800

    init

---
+++ Booking.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Booking
+{
+    private String bookingId;
+    private String movieId;
+    private String cinemaName;
+    private String cinemaLocation;
+    private String accountId;
+    private int quantityAdult;
+    private int quantityChildren;
+    private double priceAdult = 12.90;
+    private double priceChildren = 6.90;
+    private String time;
+    private Date date;
+}
