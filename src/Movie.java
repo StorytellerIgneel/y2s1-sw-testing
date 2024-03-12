60643c603d537693d32ebe1bc29caedafbf2d437
StorytellerIgneel <teohwh2004@gmail.com>
2024-03-12 22:56:04 +0800

    modified movie

--- src/Movie.java
+++ src/Movie.java
@@ -1,64 +1,115 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
 public class Movie {
+    private String movieId;
     private String title;
-    private Duration duration;
-    private String genre;
-    private ArrayList<String> languages;
+    private String description;
+    private Date[] showtimes;
+    private String[] languages;
+    private String releaseDate;
+    private String[] genre;
+    private double priceAdult;
+    private double priceChildren;
 
-    // Getters and setters
+    // Constructor
+    public Movie(String movieId, String title, String description, Date[] showtimes, String[] languages, String releaseDate, String[] genre, double priceAdult, double priceChildren) {
+        this.movieId = movieId;
+        this.title = title;
+        this.description = description;
+        this.showtimes = showtimes;
+        this.languages = languages;
+        this.releaseDate = releaseDate;
+        this.genre = genre;
+        this.priceAdult = priceAdult;
+        this.priceChildren = priceChildren;
+    }
+
+    // Getter and Setter methods
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
     public String getTitle() {
         return title;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public ArrayList<String> getLanguages() {
-        return languages;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setDuration(Duration duration) {
-        this.duration = duration;
+    public String getDescription() {
+        return description;
     }
 
-    public void setGenre(String genre) {
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date[] getShowtimes() {
+        return showtimes;
+    }
+
+    public void setShowtimes(Date[] showtimes) {
+        this.showtimes = showtimes;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String[] getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String[] genre) {
         this.genre = genre;
     }
 
-    public void setLanguages(ArrayList<String> languages) {
-        this.languages = languages;
-    }
-}
-
-class Duration {
-    private int hours;
-    private int minutes;
-
-    // Getters
-    public int getHours() {
-        return hours;
-    }
-    
-    public int getMinutes() {
-        return minutes;
+    public double getPriceAdult() {
+        return priceAdult;
     }
 
-    //setters
-    public void setHours(int hours) {
-        this.hours = hours;
+    public void setPriceAdult(double priceAdult) {
+        this.priceAdult = priceAdult;
     }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
+    public double getPriceChildren() {
+        return priceChildren;
+    }
+
+    public void setPriceChildren(double priceChildren) {
+        this.priceChildren = priceChildren;
+    }
+
+    //other methods
+    public String viewInformation() {
+        StringBuilder movie_info = new StringBuilder();
+        movie_info.append("\n\nMovie ID: ").append(movieId).append("\n");
+        movie_info.append("Title: ").append(title).append("\n");
+        movie_info.append("Description: ").append(description).append("\n");
+        movie_info.append("Showtimes: ").append(Arrays.toString(showtimes)).append("\n");
+        movie_info.append("Languages: ").append(Arrays.toString(languages)).append("\n");
+        movie_info.append("Release Date: ").append(releaseDate).append("\n");
+        movie_info.append("Genre: ").append(Arrays.toString(genre)).append("\n");
+        movie_info.append("Price (Adult): $").append(priceAdult).append("\n");
+        movie_info.append("Price (Children): $").append(priceChildren).append("\n");
+        return movie_info.toString();
     }
 }
