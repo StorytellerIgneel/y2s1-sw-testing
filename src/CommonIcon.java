ae0ee44f0d0175e6a4b6b3108e29195ab058bd5e
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-03-24 12:05:59 +0800

    modified naming

--- src/CommonIcon.java
+++ src/CommonIcon.java
@@ -1,12 +1,12 @@
 public class CommonIcon {
     public static void printHeader()
     {
-        printDashes('-', 60);
+        printChar('-', 60);
         System.out.println("TVG Cinemas");
-        printDashes('-', 60);
+        printChar('-', 60);
     }
 
-    public static void printDashes(char ch, int times)
+    public static void printChar(char ch, int times)
     {
         for (int i = 0; i < times; i++)
         {
