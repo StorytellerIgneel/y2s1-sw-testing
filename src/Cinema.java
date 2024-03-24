fb2659f6ce98c4dfb6413877d729d906a65d458f
AmIWhatIAm <96875017+AmIWhatIAm@users.noreply.github.com>
2024-03-24 22:47:25 +0800

    Modified Cinema with , added CinemaPage

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,32 +1,66 @@
+import java.util.ArrayList;
+
 public class Cinema 
 {
-    // instance variables
+    // variables
     private String cinemaName;
-    private String cinemaLocation;
-    private Movie[] moviesAired;
+    private String cinemaAddress;
+    private ArrayList<Movie> moviesAired;
+    // mock data
+    private static Cinema[] cinemaLocation = 
+    {
+        new Cinema("TVG Cheras Sentral", "Level 9, Cheras Sentral Shopping Mall Jalan 2/142A, Taman Len Seng, 56000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur, Malaysia."),
+        new Cinema("TVG Strand Kota Damansara", "Level 2, Jalan PJU 5/22, Encorp Strand Mall, Pusat Perdagangan, Kota Damansara, 47810 Petaling Jaya, Selangor, Malaysia.")
+    };
 
-    // constructor
-    public Cinema(String cinemaName, String cinemaLocation, Movie[] moviesAired) 
+    // constructors
+    public Cinema(String cinemaName, String cinemaAddress)
     {
         this.cinemaName = cinemaName;
-        this.cinemaLocation = cinemaLocation;
+        this.cinemaAddress = cinemaAddress;
+    }
+
+    public Cinema(String cinemaName, String cinemaAddress, ArrayList<Movie> moviesAired) 
+    {
+        this.cinemaName = cinemaName;
+        this.cinemaAddress = cinemaAddress;
+        this.moviesAired = moviesAired;
+    }
+    
+    // accessors
+    public String getCinemaName()
+    {
+        return cinemaName;
+    }
+    
+    public String getCinemaAddress()
+    {
+        return cinemaAddress;
+    }
+
+    public static Cinema[] getCinemaLocation()
+    {
+        return cinemaLocation;
+    }
+
+    public ArrayList<Movie> getMoviesAired()
+    {
+        return moviesAired;
+    }
+
+    public void setMoviesAired(ArrayList<Movie> moviesAired)
+    {
         this.moviesAired = moviesAired;
     }
 
     // methods
-    public String viewLocation() 
+    public void viewLocation() 
     {
-        return "Location";
+       
     }
 
     public void viewMovieInfo() 
     {
 
     }
-
-    public void viewCinema()
-    {
-
-    }
-
 }
