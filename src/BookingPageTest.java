99daf077e5dd907cddb4a6c6a45b1929c2af3695
Sodiumchloridy <bryanlee13579@gmail.com>
2024-03-31 21:48:31 +0800

    Refactor booking page and add movie objects

--- src/BookingPageTest.java
+++ src/BookingPageTest.java
@@ -1,22 +1,50 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import account.UserAccount;
 import booking.Booking;
+import cinema.Cinema;
+import movie.Movie;
 import ui.BookingPage;
 
 public class BookingPageTest {
     public static void main(String[] args)
     {
         UserAccount user = new UserAccount("John", "Doe", "password", "01-01-2021");
-        Booking booking = new Booking("Avengers: Endgame", "TVG Cheras Selatan", "Cheras", 2, 1, "03-01-2024 10:00:00");
-        Booking booking2 = new Booking("Kung Fu Panda 4", "TVG Gurney Drive", "George Town", 2, 0, "05-02-2024 22:30:00");
+        Movie movie1 = new Movie(
+            "avengers-endgame", 
+            "Avengers: Endgame", 
+            "The Avengers take a final stand against Thanos.",
+            new ArrayList<>(Arrays.asList("03-01-2024 10:00:00", "03-01-2024 13:00:00", "03-01-2024 16:00:00")),
+            new ArrayList<String>(),
+            "26-04-2019",
+            new ArrayList<String>(),
+            15.00,
+            10.00
+        );
+        Movie movie2 = new Movie(
+            "spiderman-far-from-home", 
+            "Spider Man: Far From Home",
+            "Peter Parker goes on a trip to Europe.",
+            new ArrayList<>(Arrays.asList("05-06-2024 22:00:00", "03-01-2024 13:00:00", "03-01-2024 16:00:00")),
+            new ArrayList<String>(),
+            "02-07-2019",
+            new ArrayList<String>(),
+            15.00,
+            10.00
+        );
+
+        Booking booking = new Booking(movie1, Cinema.getCinemaLocation()[0], "03-01-2024 10:00:00", 2, 3);
+        Booking booking2 = new Booking(movie2, Cinema.getCinemaLocation()[1], "05-06-2024 22:00:00", 2, 0);
         
         ArrayList<Booking> bookings = new ArrayList<Booking>();
         bookings.add(booking);
         bookings.add(booking2);
 
         user.setBookings(bookings);
-        BookingPage bookingPage = new BookingPage(user);
+        Scanner scanner = new Scanner(System.in);
+        BookingPage bookingPage = new BookingPage(user, scanner);
         bookingPage.display();
     }
 }
--- src/ui/BookingPage.java
+++ src/ui/BookingPage.java
@@ -1,35 +1,27 @@
 package ui;
+
+import java.util.Scanner;
+
 import account.UserAccount;
-import booking.Booking;
+import booking.BookingController;
 import color.Color;
 import util.CommonIcon;
 
 public class BookingPage {
     private UserAccount user;
+    private Scanner scanner;
+    private BookingController bookingController;
 
     /**
      * Constructor for BookingPage for a user
      * @param user
+     * @param scanner
      */
-    public BookingPage(UserAccount user) 
+    public BookingPage(UserAccount user, Scanner scanner) 
     {
         this.user = user;
-    }
-
-    /**
-     * Prints the details of a booking
-     * @param index
-     * @param booking
-     */
-    public void printBookingDetails(int index, Booking booking)
-    {
-        System.out.println(Color.red + (index + 1) + ". \tMovie: " + Color.lime + booking.getMovieName());
-        System.out.println(Color.red + "\tShowtime: " + Color.lime + booking.getShowtime());
-        System.out.println(Color.red + "\tCinema: " + Color.lime + booking.getCinemaName());
-        System.out.println(Color.red + "\tLocation: " + Color.lime + booking.getCinemaLocation());
-        System.out.println(Color.red + "\tChildren: " + Color.lime + booking.getQuantityChildren());
-        System.out.println(Color.red + "\tAdults: " + Color.lime + booking.getQuantityAdult());
-        System.out.println(Color.red + "\tTotal Price: " + Color.lime + "RM " + booking.calculateTotalPrice());
+        this.scanner = scanner;
+        this.bookingController = new BookingController(user);
     }
 
     /**
@@ -40,22 +32,19 @@ public class BookingPage {
         CommonIcon.printHeader();
         System.out.println();
         System.out.println(Color.reset + "Welcome, " + user.getName() + "!\n");
-        System.out.println(Color.red + "Your Bookings:");
+        System.out.println(Color.reset + "Your Bookings:");
 
         // Prints all booking details
-        for (int i = 0; i < user.getBookings().size(); i++)
-        {
-            printBookingDetails(i, user.getBookings().get(i));
-            System.out.println();
-        }
+        bookingController.printAllBookings();
 
         System.out.println(Color.reset + "What would you like to do with your bookings?"); // TODO add booking controller
-        System.out.println(Color.red + "1. " + Color.lime + "Create Bookings");
-        System.out.println(Color.red + "2. " + Color.lime + "Update Bookings");
-        System.out.println(Color.red + "3. " + Color.lime + "Delete Bookings");
-        System.out.println(Color.red + "4. " + Color.lime + "Back to Main Menu");
+        System.out.println(Color.red + "1) " + Color.lime + "Create Bookings");
+        System.out.println(Color.red + "2) " + Color.lime + "Update Bookings");
+        System.out.println(Color.red + "3) " + Color.lime + "Delete Bookings");
+        System.out.println(Color.red + "4) " + Color.lime + "Back to Main Menu");
         System.out.println();
         System.out.print(Color.reset + "Enter your choice: ");
-        System.out.print(Color.reset);
+        
+        // TODO Call the booking controller
     }    
 }
