9e508f427541c81aa57e53185db0e4d05b06a276
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-04-19 22:09:41 +0800

    added more comments

--- src/account/Account.java
+++ src/account/Account.java
@@ -7,6 +7,7 @@ import util.*;
 
 import java.io.*;
 
+
 public class Account {
   //instance variables to be inherited
   protected String accountId;
@@ -27,17 +28,16 @@ public class Account {
   }
 
   //static methods
+  /**
+ * Verifies the login credentials of a user in the system.
+ * 
+ * @param accounts the list of accounts in the system
+ * @param name the username of the user
+ * @param pass the password of the user
+ * @return the index of the user in the list if the login is successful, -1 otherwise
+ */
   public static int verifyLogin(ArrayList<Account> accounts, String name, String pass)
   {
-    // for(Account account : accounts)
-    // {
-    //   if(account.getName().equals(name) && account.getPassword().equals(pass))
-    //   {
-    //     return true;
-    //   }
-    // }
-    // return false;
-
     for(int i = 0; i < accounts.size(); i++)
     {
       if(accounts.get(i).getName().equals(name) && accounts.get(i).getPassword().equals(pass))
@@ -45,7 +45,14 @@ public class Account {
     }
     return -1;
   }
-  
+  /**
+ * This method is used to handle the login process of the user. It takes in the list of accounts and prompts the user to enter their username and password.
+ * If the login is successful, the index of the user is returned, 
+ * otherwise -1 is returned for back and -2 is returned for quit.
+ * 
+ * @param accounts the list of accounts used to verify the login process
+ * @return the index of the user in the list if the login is successful
+ */
   public static int login(ArrayList<Account> accounts)
  {
     Scanner input = new Scanner(System.in);
