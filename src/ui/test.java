9b7b46ea3360624ed9ac9477d995e07a14f672d5
StorytellerIgneel <teohwh2004@gmail.com>
2024-04-20 22:14:48 +0800

    regular commit

--- src/ui/test.java
+++ src/ui/test.java
@@ -1,12 +1,13 @@
 package ui;
 
 import java.util.Scanner;
-
+import movie.Movie;
+import java.util.ArrayList;
 // TODO remove file in production
 public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        MovieCRUDGeneralPage moviePage = new MovieCRUDGeneralPage(scanner);
-        moviePage.MainPage();
+        ArrayList<Movie> movieList = MovieCRUDGeneralPage.getMovieList();
+        MovieCRUDGeneralPage.sortReleaseDate(movieList);
     }
 }
