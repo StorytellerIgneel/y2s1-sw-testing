e8d295b83d98bc079da39e579f9bf47efc25ab64
Sodiumchloridy <bryanlee13579@gmail.com>
2024-04-21 10:07:28 +0800

    Refactor Makefile, test.java, admin.json, user.json, and GenerateReport.java

--- src/ui/systemAdmin/GenerateReport.java
+++ src/ui/systemAdmin/GenerateReport.java
@@ -6,25 +6,25 @@ import util.CommonIcon;
 import report.*;
 
 public class GenerateReport {
-    public static void printReport(ArrayList<UserAccount> users)
-    {
+    public static void printReport(ArrayList<UserAccount> users) {
         ArrayList<Report> data = new ArrayList<Report>();
         Report.generateTicketSalesReport(users, data);
         CommonIcon.printHeader();
         System.out.println("Monthly Report");
         CommonIcon.printChar('-', 60);
-        System.out.println("Report Month: "); // 
+        System.out.println("Report Month: "); //
         CommonIcon.printChar('-', 60);
         System.out.println("*A = Adult  C = Children");
-        System.out.printf("%-3s %-9s\t%-30s\t%-3s %-3s %10s\n", "No", "Movie ID", "Movie Name", "A*", "C*", "Revenue");
+        System.out.printf("%-3s %-9s\t%-30s\t%-3s %-3s %10s\n", "No", "Movie ID", "Movie Name",
+                "A*", "C*", "Revenue");
         double totalRevenue = 0;
-        for (int i = 0; i < data.size(); i++)
-        {
+        for (int i = 0; i < data.size(); i++) {
             Report current = data.get(i);
             double revenue = current.calculateTotalRevenue();
-            totalRevenue+=revenue;
-            System.out.printf("%02d. %-9s\t%-30s\t%-3s %-3s %10.2f\n", 
-            i+1, current.getMovieId(), current.getMovieName(), current.getTotalAdult(), current.getTotalChildren(), revenue);
+            totalRevenue += revenue;
+            System.out.printf("%02d. %-9s\t%-30s\t%-3s %-3s %10.2f\n", i + 1, current.getMovieId(),
+                    current.getMovieName(), current.getTotalAdult(), current.getTotalChildren(),
+                    revenue);
         }
         CommonIcon.printChar('-', 60);
         System.out.printf("%55s %10.2f", "Total Revenue:", totalRevenue);
