83b253622f37096b67c27124b2f20436ea8fba28
rong <chongrq123@gmail.com>
2024-07-22 08:37:37 +0800

    sync

--- src/testing.java
+++ src/testing.java
@@ -1,17 +1,16 @@
+import FileHandling.FileHandling;
+import booking.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import FileHandling.FileHandling;
-import movie.*;
 
 public class testing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        ArrayList<Movie> movies = FileHandling.getMovieList();
-        for (Movie movie : movies) {
-            System.out.println(movie.getTitle());
-        FileHandling.exportMovieData(movies, null);
+        ArrayList<Booking> bookings = FileHandling.getBookingList();
+        for (Booking booking : bookings) {
+            System.out.println(booking.getBookingId()+booking.getStatus());
+        FileHandling.exportBookingData(bookings, null);
         }
 
         
